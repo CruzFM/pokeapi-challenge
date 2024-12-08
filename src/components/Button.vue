502090cf791d20ee8bda8cb755cafc1e27f923e8
Fer Cruz <cruzfernandomartin94@gmail.com>
2024-12-08 12:12:51 -0300

    Button Styling and validation corrected: styling when only an icon is passed with no text fixed. Validation for handleClick prop fixed, developers should pass a handleClick if Button is intended to be a button and not a link.

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -10,7 +10,7 @@
         :class="computedClass"
         :active="isActive"
     >
-        <img v-if="icon" :src="icon" alt="icon" class="mr-2 w-[22px]"/>
+        <img v-if="icon" :src="icon" alt="icon" class="w-[22px]"/>
         {{ text }}
     </button>
 </template>
@@ -29,11 +29,8 @@ export default {
         },
         handleClick: {
             type: Function,
-            validator: function (value) {
-                if (this.to === null){
-                    return value !== undefined;
-                }
-                return true;
+            validator(value, props){
+                return !(props.to === undefined && value === undefined)
             }
         },
         text: String,
@@ -53,23 +50,27 @@ export default {
     data(){
         return{
             classes: {
-                base: "w-[141px] rounded-[60px] py-2 px-5 font-semibold text-white flex justify-center items-center text-lg",
+                base: "font-semibold text-white flex justify-center items-center text-lg",
                 primary: "bg-[#f22539] hover:bg-[#c00e20]",
-                secondary: "bg-[#bfbfbf] hover:bg-[#7E7B7B]"
+                secondary: "bg-[#bfbfbf] hover:bg-[#7E7B7B]",
+                widthText: "py-2 px-5 w-[141px] rounded-[60px] gap-2",
+                iconOnlyWidth: "p-2 w-auto h-auto rounded-full"
             }
         }
     },
     computed: {
         computedClass(){
-            let base = this.classes.base
-            let variantClass = this.classes[this.variant]
+            let sizeClass = this.text
+                ? this.classes.widthText
+                : this.classes.iconOnlyWidth
+            let baseClass = `${this.classes.base} ${sizeClass}`;
             if(this.icon){
                 return this.isActive 
-                    ? `${base} ${this.classes.primary}`
-                    : `${base} ${this.classes.secondary}`;
+                    ? `${baseClass} ${this.classes.primary}`
+                    : `${baseClass} ${this.classes.secondary}`;
             }
-            return `${base} ${variantClass}` 
-        }
-    }
+            return `${baseClass} ${this.classes[this.variant]}`;
+        },
+    },
 }
 </script>
